1770af2f0512a996920b4d59561e3490d2adf684
Danylo Holets <goletsdan@gmail.com>
2023-03-01 16:51:09 +0200

    Refactored output

--- src/quadEquation.js
+++ src/quadEquation.js
@@ -5,19 +5,16 @@ const quadEquation = (a, b, c) => {
     const disc = b * b - 4 * a * c;
     if(disc === 0){
         const x = -b / (2 * a);
-        console.log(`There are 1 root:`);
-        console.log(`x = ${x}`);
+        console.log(`There are 1 root:\nx = ${x}`);
         return;
     }
     if(disc > 0){
         const x1 = (-b - Math.sqrt(disc)) / (2 * a);
         const x2 = (-b + Math.sqrt(disc)) / (2 * a);
-        console.log(`There are 2 roots:`);
-        console.log(`x1 = ${x1}`);
-        console.log(`x2 = ${x2}`);
+        console.log(`There are 2 roots:\nx1 = ${x1}\nx2 = ${x2}`);
         return;
     }
     console.log(`There are no roots`);
 };
 
-quadEquation(1, 0 , 9);
+quadEquation(2, 1, -3);
