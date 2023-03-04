b195c138d05880c7727db80d9cf2a8ae62df07f3
Danylo Holets <goletsdan@gmail.com>
2023-03-04 15:43:10 +0200

    Added exit codes

--- src/fileMode.js
+++ src/fileMode.js
@@ -3,26 +3,26 @@ import { quadEquation } from './quadEquation.js';
 
 const fileMode = path => {
   if (!existsSync(path)) {
-    console.log('Invalid file path');
-    return;
+    console.log(`Error. File ${path} does not exist`);
+    process.exit(404);
   }
   const data = readFileSync(path).toString();
   const format = /^(.*)\s(.*)\s(.*)(\r)?\n$/;
   if (!data.match(format)) {
     console.log('Error. Invalid data format');
-    return;
+    process.exit(400);
   }
   let params = data.trim('\n').split(' ');
   for (const param of params) {
     if (isNaN(Number(param))) {
       console.log(`Error. Expected a valid real number, got ${param} instead`);
-      return;
+      process.exit(400);
     }
   }
   params = params.map(param => Number(param));
   if (params[0] === 0) {
     console.log('Error. a cannot be 0');
-    return;
+    process.exit(400);
   }
   quadEquation(params[0], params[1], params[2]);
 };
