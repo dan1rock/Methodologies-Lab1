fe1ccaff5cb76aeb5da9880c9721d2fe614d73b7
Danylo Holets <goletsdan@gmail.com>
2023-03-04 15:34:23 +0200

    Added tests and improved fileMode

--- src/fileMode.js
+++ src/fileMode.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { readFileSync, existsSync } from 'fs';
 import { quadEquation } from './quadEquation.js';
 
@@ -8,19 +7,19 @@ const fileMode = path => {
     return;
   }
   const data = readFileSync(path).toString();
-  // eslint-disable-next-line max-len
-  const format = /^([+-]?([0-9]*[.])?[0-9]+)\s([+-]?([0-9]*[.])?[0-9]+)\s([+-]?([0-9]*[.])?[0-9]+)(\r)?\n$/;
+  const format = /^(.*)\s(.*)\s(.*)(\r)?\n$/;
   if (!data.match(format)) {
     console.log('Error. Invalid data format');
     return;
   }
-  const params = data.trim('\n').split(' ');
+  let params = data.trim('\n').split(' ');
   for (const param of params) {
     if (isNaN(Number(param))) {
       console.log(`Error. Expected a valid real number, got ${param} instead`);
       return;
     }
   }
+  params = params.map(param => Number(param));
   if (params[0] === 0) {
     console.log('Error. a cannot be 0');
     return;
